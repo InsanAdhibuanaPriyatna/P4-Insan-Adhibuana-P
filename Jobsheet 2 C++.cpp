f84052dba087995f4b9e20ee685250050b7dc5b7
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-11 20:31:22 -0700

    Create Jobsheet 2 C++.cpp
    Mengisi dan Menampilkan Elemen Aray (Dinamis) dengan index array sesuai dengan 2 digit NIM terakhir mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,50 @@
+#include <iostream> 
+using namespace std; 
+
+main(){
+cout << "Nama : Insan Adhibuana Priyatna\n";
+cout << "NIM : F1B019073\n";
+cout << "Kelompok : 15\n";
+
+string x[13]; 
+cout<<"masukkan nim 1 = ";
+cin>>x[0]; 
+cout<<"masukkan nim 2 = ";
+cin>>x[1];
+cout<<"masukkan nim 3 = "; 
+cin>>x[2];
+cout<<"masukkan nim 4 = "; 
+cin>>x[3];
+cout<<"masukkan nim 5 = "; 
+cin>>x[4];
+cout<<"masukkan nim 6 = "; 
+cin>>x[5];
+cout<<"masukkan nim 7 = "; 
+cin>>x[6];
+cout<<"masukkan nim 8 = "; 
+cin>>x[7];
+cout<<"masukkan nim 9 = "; 
+cin>>x[8];
+cout<<"masukkan nim 10 = "; 
+cin>>x[9];
+cout<<"masukkan nim 11 = "; 
+cin>>x[10];
+cout<<"masukkan nim 12 = "; 
+cin>>x[11];
+cout<<"masukkan nim 13 = "; 
+cin>>x[12];
+
+cout <<"nim 1 = "<<x[0]<<"\n"; 
+cout <<"nim 2 = "<<x[1]<<"\n"; 
+cout <<"nim 3 = "<<x[2]<<"\n"; 
+cout <<"nim 4 = "<<x[3]<<"\n"; 
+cout <<"nim 5 = "<<x[4]<<"\n"; 
+cout <<"nim 6 = "<<x[5]<<"\n"; 
+cout <<"nim 7 = "<<x[6]<<"\n"; 
+cout <<"nim 8 = "<<x[7]<<"\n"; 
+cout <<"nim 9 = "<<x[8]<<"\n"; 
+cout <<"nim 10 = "<<x[9]<<"\n"; 
+cout <<"nim 11 = "<<x[10]<<"\n"; 
+cout <<"nim 12 = "<<x[11]<<"\n"; 
+cout <<"nim 13 = "<<x[12]<<"\n";
+}
