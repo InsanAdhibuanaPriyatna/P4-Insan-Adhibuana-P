8844973cbd8240cb406780deba97cd5f1488c1d2
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-11 20:22:08 -0700

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi dengan jumlah data sebanyak 2 digit NIM terakhir mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std;
+
+int main (){
+cout << "Nama : Insan Adhibuana Priyatna\n";
+cout << "NIM : F1B019073\n";
+cout << "Kelompok : 15\n";
+
+int nim[13]={1,3,6,5,4,51,226,76,35,34,32,12,11};
+cout <<"nim mahasiswa ke- 1 = "<<nim[0]<<endl;
+cout <<"nim mahasiswa ke- 2 = "<<nim[1]<<endl;
+cout <<"nim mahasiswa ke- 3 = "<<nim[2]<<endl;
+cout <<"nim mahasiswa ke- 4 = "<<nim[3]<<endl;
+cout <<"nim mahasiswa ke- 5 = "<<nim[4]<<endl;
+cout <<"nim mahasiswa ke- 6 = "<<nim[5]<<endl;
+cout <<"nim mahasiswa ke- 7 = "<<nim[6]<<endl;
+cout <<"nim mahasiswa ke- 8 = "<<nim[7]<<endl;
+cout <<"nim mahasiswa ke- 9 = "<<nim[8]<<endl;
+cout <<"nim mahasiswa ke- 10 = "<<nim[9]<<endl;
+cout <<"nim mahasiswa ke- 11 = "<<nim[10]<<endl;
+cout <<"nim mahasiswa ke- 12 = "<<nim[11]<<endl;
+cout <<"nim mahasiswa ke- 13 = "<<nim[12]<<endl;
+}
