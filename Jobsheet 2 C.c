d412205b9fd1dc4b594cf83ad3e892eaeb16cceb
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-11 20:28:16 -0700

    Create Jobsheet 2 C.c
    Mengisi dan Menampilkan Elemen Aray (Dinamis) dengan index array sesuai dengan 2 digit NIM terakhir mahasiswa

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+printf("Nama : Insan Adhibuana Priyatna\n");
+printf("NIM : F1B019073\n"); 
+printf("Kelompok : 15\n");
+
+int x [13];
+printf("masukkan nim 1 = "); 
+scanf("%s",&x[0]); 
+printf("masukkan nim 2 = ");
+scanf("%s",&x[1]);
+printf("masukkan nim 3 = "); 
+scanf("%s",&x[2]); 
+printf("masukkan nim 4 = "); 
+scanf("%s",&x[3]); 
+printf("masukkan nim 5 = "); 
+scanf("%s",&x[4]); 
+printf("masukkan nim 6 = "); 
+scanf("%s",&x[5]); 
+printf("masukkan nim 7 = "); 
+scanf("%s",&x[6]); 
+printf("masukkan nim 8 = "); 
+scanf("%s",&x[7]); 
+printf("masukkan nim 9 = "); 
+scanf("%s",&x[8]); 
+printf("masukkan nim 10 = "); 
+scanf("%s",&x[9]); 
+printf("masukkan nim 11 = "); 
+scanf("%s",&x[10]); 
+printf("masukkan nim 12 = "); 
+scanf("%s",&x[11]); 
+printf("masukkan nim 13 = "); 
+scanf("%s",&x[12]);
+
+printf("nim 1 = %s\n",&x[0]);
+printf("nim 2 = %s\n",&x[1]);
+printf("nim 3 = %s\n",&x[2]);
+printf("nim 4 = %s\n",&x[3]);
+printf("nim 5 = %s\n",&x[4]);
+printf("nim 6 = %s\n",&x[5]);
+printf("nim 7 = %s\n",&x[6]);
+printf("nim 8 = %s\n",&x[7]);
+printf("nim 9 = %s\n",&x[8]); 
+printf("nim 10 = %s\n",&x[9]); 
+printf("nim 11 = %s\n",&x[10]); 
+printf("nim 12 = %s\n",&x[11]); 
+printf("nim 13 = %s\n",&x[12]);
+}
