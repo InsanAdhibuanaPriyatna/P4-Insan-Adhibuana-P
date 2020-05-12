7154d11f081078c47401dcddbeb8cb71228d6683
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-11 21:25:40 -0700

    Create Jobsheet 6 C++.cpp
    Program matriks transpose bersifat dinamis

---
+++ Jobsheet 6 C++.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std; 
+
+main (){
+int i, j;
+int matrik1[2][2];
+for (i = 0; i < 3; i++){
+  for (j=0; j<3; j++){
+  cout << "Masukkan nilai matrik " << i << "," << j << " = ";
+  cin >> matrik1[i][j];
+  }
+}
+cout << endl;
+for (i=0; i< 3; i++){
+  for (j = 0; j < 3; j++){ 
+  cout<<matrik1[i][j]<<" ";
+  }
+  cout << endl;
+}
+cout << endl;
+cout<<"nilai matrik setelah di transpose = "<<endl;
+for (i=0; i< 3; i++){
+  for (j = 0; j < 3; j++){
+  cout<<matrik1[j][i]<<" ";
+  }
+cout << endl;
+ }
+}
