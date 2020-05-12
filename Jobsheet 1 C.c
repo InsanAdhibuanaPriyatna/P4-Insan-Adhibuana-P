241408ae369491220063e04fa76cf6b02637118a
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-11 20:18:38 -0700

    Create Jobsheet 1 C.c
    Menampilkan array satu dimensi dengan jumlah data sebanyak 2 digit NIM terakhir mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+main(){
+printf("Nama : Insan Adhibuana Priyatna\n");
+printf("NIM : F1B019073\n"); 
+printf("Kelompok : 15\n");
+
+int nim[13]={1,3,6,5,4,51,226,76,35,34,32,12, 11};
+printf("nim mahasiswa ke- 1 = %i\n",nim[0]);
+printf("nim mahasiswa ke- 2 = %i\n",nim[1]);
+printf("nim mahasiswa ke- 3 = %i\n",nim[2]);
+printf("nim mahasiswa ke- 4 = %i\n",nim[3]);
+printf("nim mahasiswa ke- 5 = %i\n",nim[4]);
+printf("nim mahasiswa ke- 6 = %i\n",nim[5]);
+printf("nim mahasiswa ke- 7 = %i\n",nim[6]);
+printf("nim mahasiswa ke- 8 = %i\n",nim[7]);
+printf("nim mahasiswa ke- 9 = %i\n",nim[8]);
+printf("nim mahasiswa ke- 10 = %i\n",nim[9]);
+printf("nim mahasiswa ke- 11 = %i\n",nim[10]);
+printf("nim mahasiswa ke- 12 = %i\n",nim[11]);
+printf("nim mahasiswa ke- 13 = %i\n",nim[12]);
+}
